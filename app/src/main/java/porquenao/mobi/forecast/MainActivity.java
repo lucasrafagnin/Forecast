5d38459cdf533cff6aa6c7cd656b032fc3db52f9
Lucas Rafagnin <lucas@porquenao.mobi>
2015-07-09 14:49:09 -0300

    added libs: calligraphy, picasso and PagerSlidingTabStrip. Created font styles.

--- app/src/main/java/porquenao/mobi/forecast/MainActivity.java
+++ app/src/main/java/porquenao/mobi/forecast/MainActivity.java
@@ -1,37 +1,25 @@
 package porquenao.mobi.forecast;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends ActionBarActivity {
+import com.astuetz.PagerSlidingTabStrip;
+
+import porquenao.mobi.forecast.ui.MainAdapter;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MainAdapter(getSupportFragmentManager()));
+
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs_bar);
+        tabs.setViewPager(pager);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
